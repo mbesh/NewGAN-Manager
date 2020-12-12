2bbf95f4f20e3996813180761ce04fec90075e36
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2020-12-12 10:56:45 +0100

    minimal example

--- src/newganmanager/app.py
+++ src/newganmanager/app.py
@@ -8,12 +8,20 @@ import time
 class NewGANManager(toga.App):
 
     def startup(self):
+        self.main_window = toga.MainWindow(self.name)
         self.output = toga.Label("")
         self.btn = toga.Button(label="Run", on_press=self.calc)
-
+        self.main_box = toga.Box()
+        self.main_box.add(self.output)
+        self.main_box.add(self.btn)
+        self.main_window.content = self.main_box
+        self.main_window.show()
     def calc(self, widget):
         self.output.text = "RUN1"
         yield 0.1
         time.sleep(3)
         self.output.text = "RUN2"
         yield 0.1
+
+def main():
+    return NewGANManager('Toga Demo', 'org.beeware.toga-demo')
