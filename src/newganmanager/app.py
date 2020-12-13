27c9a61ac75d3143c175ccbdf274f76e208e65af
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2020-12-13 13:20:09 +0100

    Keith fix

--- src/newganmanager/app.py
+++ src/newganmanager/app.py
@@ -3,13 +3,13 @@ Yield Test
 """
 import toga
 import time
-
+from toga.style.pack import Pack
 
 class NewGANManager(toga.App):
 
     def startup(self):
         self.main_window = toga.MainWindow(self.name)
-        self.output = toga.Label("")
+        self.output = toga.Label("", style=Pack(width=100))
         self.btn = toga.Button(label="Run", on_press=self.calc)
         self.main_box = toga.Box()
         self.main_box.add(self.output)
